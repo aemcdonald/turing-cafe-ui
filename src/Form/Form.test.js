83c4b4e9d62ba0f7217f44e11daca806ef2760d9
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-10-21 11:31:15 -0600

    Add test for form inputs functionality

--- src/Form/Form.test.js
+++ src/Form/Form.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Form from './Form';
-import { screen, render, waitFor } from '@testing-library/react';
+import { screen, render, waitFor, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 
 describe('Form', () => {
@@ -15,4 +16,27 @@ describe('Form', () => {
     expect(screen.getByPlaceholderText('Number of guests')).toBeInTheDocument();
     expect(screen.getByText('Make Reservation')).toBeInTheDocument();
   })
-})
+
+  it('Should allow a user to fill out the inputs', () => {
+    const mockResFunction = jest.fn();
+    render(<Form handleReservation={mockResFunction}/>);
+
+    const nameInput = screen.getByPlaceholderText('Name');
+    const dateInput = screen.getByPlaceholderText('Date (mm/d)');
+    const timeInput = screen.getByPlaceholderText('Time');
+    const numberInput = screen.getByPlaceholderText('Number of guests');
+    const button = screen.getByText('Make Reservation');
+
+    fireEvent.change(nameInput, {target: {value: 'Ashley'}});
+    fireEvent.change(dateInput, {target: {value: '10/1'}});
+    fireEvent.change(timeInput, {target: {value: '11:00'}});
+    fireEvent.change(numberInput, {target: {value: '4'}});
+    fireEvent.click(button);
+
+    expect(nameInput.value).toBe('Ashley');
+    expect(dateInput.value).toBe('10/1');
+    expect(timeInput.value).toBe('11:00');
+    expect(numberInput.value).toBe('4');
+    expect(mockResFunction).toHaveBeenCalledTimes(1);
+  });
+});
