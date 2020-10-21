b83af263f7663c7cea407bc4838e5bdfebb85f84
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-10-21 11:03:55 -0600

    Fix id to be easier to test

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -2,8 +2,9 @@ import React from 'react';
 import './Card.css';
 
 const Card = ({reservation}) => {
+  console.log(reservation);
   return(
-    <section key={reservation.i}>
+    <section key={reservation.id}>
       <h3>Name: {reservation.name}</h3>
       <p>Date: {reservation.date}</p>
       <p>Time: {reservation.time}</p>
