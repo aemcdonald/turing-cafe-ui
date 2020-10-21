c8c86953ba3bace7fcccf69163dae98dccd4042e
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-10-21 11:11:48 -0600

    Add test for render Form

--- src/Form/Form.test.js
+++ src/Form/Form.test.js
@@ -5,7 +5,14 @@ import { screen, render, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 describe('Form', () => {
-  it('Should work', () => {
-    expect(true).toBe(true)
+  it('Should render a reservation form', () => {
+    const mockFunction = jest.fn()
+    render(<Form handleReservation={mockFunction}/>)
+
+    expect(screen.getByPlaceholderText('Name')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Date (mm/d)')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Time')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Number of guests')).toBeInTheDocument();
+    expect(screen.getByText('Make Reservation')).toBeInTheDocument();
   })
 })
