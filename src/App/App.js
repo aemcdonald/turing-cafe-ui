051746b879b082c89e2ca50f166837e76612184e
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-10-21 09:35:37 -0600

    Add code to ensure form hooked up properly

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import Form from '../Form/Form.js';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {}
   }
-  
+
   render() {
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
+          <Form />
         </div>
         <div className='resy-container'>
 
