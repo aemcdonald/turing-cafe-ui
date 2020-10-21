9f456dbdfe78a7ff8173dae4be08e9fcbeb44552
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-10-21 11:03:19 -0600

    Add test for card

--- src/Card/Card.test.js
+++ src/Card/Card.test.js
@@ -5,7 +5,22 @@ import { screen, render, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 describe('Card', () => {
-  it('Should work', () => {
-    expect(true).toBe(true)
+  it('Should render a reservation card with the correct information', () => {
+    const fakeReservation = {
+      id: 1,
+      name: 'Ashley',
+      date: '10/21',
+      time: '10:00',
+      number: 2
+    }
+    render(
+      <Card
+        reservation={fakeReservation}
+      />);
+
+  expect(screen.getByText('Name: Ashley')).toBeInTheDocument();
+  expect(screen.getByText('Date: 10/21')).toBeInTheDocument();
+  expect(screen.getByText('Time: 10:00')).toBeInTheDocument();
+  expect(screen.getByText('Number of guests: 2')).toBeInTheDocument();
   })
 })
